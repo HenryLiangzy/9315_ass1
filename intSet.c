454ff6e774d0ee8ab0b4bb4bc222648d0a2241c9
Henry Leung <henryliangzy@gmail.com>
2021-03-15 15:56:50 +1100

    repo initilize

---
+++ intSet.c
@@ -0,0 +1,30 @@
+#include "postgres.h"
+
+#include "fmgr.h"
+#include "libpq/pqformat.h"		/* needed for send/recv functions */
+
+#include "string.h"
+#include "ctype.h"
+
+PG_MODULE_MAGIC;
+
+typedef struct intSet
+{
+    /* data */
+}intSet;
+
+
+PG_FUNCTION_INFO_V1(intset_in):
+
+Datum
+inset_in(PG_FUNCTION_ARGS){
+    char *str = PG_GETARG_CSTRING(0);
+    intSet *result;
+}
+
+PG_FUNCTION_INFO_V1(intset_out):
+
+Datum
+inset_out(PG_FUNCTION_ARGS){
+
+}
