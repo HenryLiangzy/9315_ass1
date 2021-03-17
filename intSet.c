9f0f0bf30d9d362f11dadd219c31519702329a51
Henry Leung <henryliangzy@gmail.com>
2021-03-18 02:20:54 +1100

    finish basic framework

--- intSet.c
+++ intSet.c
@@ -17,12 +17,72 @@ typedef struct intSet
 {
     int32 length;
     int32 array[FLEXIBLE_ARRAY_MEMBER];
+    int array_size;
 }intSet;
 
 
 /**
- * Structure function below
+ * Sort function below
 */
+
+void merge(int *arr, int l, int m, int r){
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    int left[n1], right[n2];
+
+    //copy
+    for(i = 0; i < n1; i++){
+        left[i] = arr[l + i];
+    }
+    for(j = 0; j < n2; j++){
+        right[j] = arr[m+1 + j];
+    }
+
+    i = 0;
+    j = 0;
+    k = l;
+
+    //combine
+    while (i < n1 && j < n2){
+        if(left[i] <= right[j]){
+            arr[k] = left[i];
+            i++;
+        }
+        else{
+            arr[k] = right[j];
+            j++;
+        }
+        k++;
+    }
+
+    // copy remain
+    while (i < n1){
+        arr[k] = left[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2){
+        arr[k] = right[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(int *arr, int l, int r){
+    if(l < r){
+        int m = (l + r) / 2;
+
+        // divide
+        mergeSort(arr, l, m);
+        mergeSort(arr, m+1, r);
+
+        //merge
+        merge(arr, l, m, r);
+    }
+}
 
 int check_valid(char *str){
     int flag = 1;
@@ -34,9 +94,9 @@ int check_valid(char *str){
         return flag;
     }
 
-    for (int i = 0; i < str_len; i++){
-        /* code */
-    }
+    // for (int i = 0; i < str_len; i++){
+    //     /* code */
+    // }
     
     return flag;
 }
@@ -69,17 +129,24 @@ inset_in(PG_FUNCTION_ARGS){
     substring = strtok(str, delim);
 
     int *temp = palloc(VARHDRSZ*input_len);
-    int size = 0;
+    int size_count = 1;
     while(substring != NULL){
-        temp[size] = atoi(substring);
+        temp[size_count] = atoi(substring);
         substring = strtok(NULL, delim);
-        size++;
+        size_count++;
     }
 
-    for (int i = 0; i < size; i++){
-        
+    // sort the array
+    mergeSort(temp, 0, size_count-1);
+
+    // allocate memory for result pointer and save data
+    // an array length (int32) + array (len * int32)
+    result = (intSet *)palloc(VARHDRSZ + VARHDRSZ * size_count);
+    SET_VARSIZE(result, VARHDRSZ + VARHDRSZ * size_count);
+    result->array_size = size_count
+    for (int i = 0; i < size_count; i++){
+        result->array[i] = temp[i];
     }
-    
 
     //free and return
     pfree(temp);
@@ -90,5 +157,62 @@ PG_FUNCTION_INFO_V1(intset_out):
 
 Datum
 inset_out(PG_FUNCTION_ARGS){
+    intSet *int_set = (intSet *) PG_GETARG_POINTER(0);
 
+    int available_len = int_set->array_size * 8;
+    int number_len = 0;
+    char number_str[32];
+    char *result = (char *)palloc(available_len + 2);
+    
+    // concate the first byte
+    strcat(result, "{");
+    sprintf(number_str, "%d", array[0]);
+    strcat(result, number_str);
+
+    //concate later string
+    for (int i = 1; i < int_set->array_size; i++){
+        strcat(result, ", ");
+        sprintf(number_str, "%d", array[i]);
+        number_len = strlen(number_str);
+        if(strlen(result) + number_len >= available_len){
+            result = realloc(result, available_len + 512);
+            available_len = available_len + 512;
+        }
+        strcat(result, number_str);
+    }
+    
+    //end
+    strcat(result, "}");
+    PG_RETURN_CSTRING(psprintf("%s", result));
+}
+
+/*****************************************************************************
+ * New Operators
+ *
+ * A practical Complex datatype would provide much more than this, of course.
+ *****************************************************************************/
+
+PG_FUNCTION_INFO_V1(intset_eq);
+
+Datum
+intset_eq(PG_FUNCTION_ARGS)
+{
+	intSet *a = (intSet *) PG_GETARG_POINTER(0);
+	intSet *b = (intSet *) PG_GETARG_POINTER(1);
+
+    int flag = 1;
+
+    if(a->array_size != b->array_size){
+        flag = 0;
+    }
+    else{
+        for (int i = 0; i < a->array_size; i++){
+            if (a->array[i] != b->array[i]){
+                flag = 0;
+                break;
+            }
+        }
+    }
+
+	PG_RETURN_BOOL(flag);
 }
